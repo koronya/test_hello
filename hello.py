3afd0e91dacca22fc0c028182807b35853181f28
koronya <koronya@hanmail.net>
2017-03-10 02:52:49 +0900

    git remote master에서 hello.py 수정

--- hello.py
+++ hello.py
@@ -3,6 +3,6 @@ print("Hello World")
 print("Tell Your world")
 print("Tell his world")   #from master
 print("Tell her world")   # from hotfix
-
+print("Tell remote world")  # from remote master
 
 
