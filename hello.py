59340a59848fd2b454b370b1def4bd34a1e4ac47
koronya <koronya@hanmail.net>
2017-03-11 13:26:35 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,3 +6,4 @@ print("Tell her world")   # from hotfix
 print("Tell local - master world")	# from master=======
 print("Tell remote world")  # from remote master
 print("Tell remote world2")
+print("Tell remote world3")
